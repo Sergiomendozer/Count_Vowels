9396bc94d693ce6fa5fc6d5cb8f29282b97b2bd6
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-18 02:25:40 -0800

    lenght in js

--- Count_Vowels.js
+++ Count_Vowels.js
@@ -1,9 +1,12 @@
+// node Count_Vowels.js
 function Count_Vowels(x)
 {var count = 0
-    var i = -1
-    console.log(string)
-    console.log(count)
-    console.log(i)
+    console.log(x.length)
+    for (var i = -1; i <= x.length; i += 1)
+    {
+        console.log(i);
+    }
+    
 }
 
 
