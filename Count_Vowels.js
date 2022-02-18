a7b90374630f4ea5ea24191febfb0066e927528f
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-18 02:56:52 -0800

    f string like

--- Count_Vowels.js
+++ Count_Vowels.js
@@ -1,18 +1,16 @@
 // node Count_Vowels.js
 function Count_Vowels(string)
-{var count = 0
-    console.log(string.length) // !delete
+{let count = 0;
     for (var i = -1; i <= string.length; i += 1)
-    {console.log(string[i]) // !delete
-        if (string[i] == "a" || string[i] == "A") {count +=1}
-        else if (string[i] == "e" || string[i] == "E") {count +=1}
+    {if (string[i] == "a" || string[i] == "A") {count +=1}
+    else if (string[i] == "e" || string[i] == "E") {count +=1}
     }
 count = String(count)
-console.log(count)
+console.log(`${string} has ${count} vowels`); // not quotes `````
 }
 
 
 // prompts user for input
 const prompt = require("prompt-sync")({ sigint: true });
-var input = prompt("Enter a word of sentence, and I will count number of vowels:");
+let input = prompt("Enter a word of sentence, and I will count number of vowels:");
 Count_Vowels(input)
