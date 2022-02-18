fb6526bac4baa508a85d2947fa5e101d7865cea9
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-18 02:04:47 -0800

    vars added

--- Count_Vowels.js
+++ Count_Vowels.js
@@ -1,11 +1,13 @@
 function Count_Vowels(x)
-{console.log(string)}
+{var count = 0
+    var i = -1
+    console.log(string)
+    console.log(count)
+    console.log(i)
+}
 
 
+// prompts user for input
 const prompt = require("prompt-sync")({ sigint: true });
-// const age = prompt("How old are you? ");
-// console.log(`You are ${age} years old.`);
-
-// node <filename>.js
 var string = prompt("Enter a word of sentence, and I will count number of vowels:");
 Count_Vowels(string)
