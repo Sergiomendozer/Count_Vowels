264b6b42960538ad7aa5dd2fd94a8b4f107f4067
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-18 02:44:13 -0800

    function working for loop returns when done

--- Count_Vowels.js
+++ Count_Vowels.js
@@ -1,16 +1,18 @@
 // node Count_Vowels.js
-function Count_Vowels(x)
+function Count_Vowels(string)
 {var count = 0
-    console.log(x.length)
-    for (var i = -1; i <= x.length; i += 1)
-    {
-        console.log(i);
+    console.log(string.length) // !delete
+    for (var i = -1; i <= string.length; i += 1)
+    {console.log(string[i]) // !delete
+        if (string[i] == "a" || string[i] == "A") {count +=1}
+        else if (string[i] == "e" || string[i] == "E") {count +=1}
     }
-    
+count = String(count)
+console.log(count)
 }
 
 
 // prompts user for input
 const prompt = require("prompt-sync")({ sigint: true });
-var string = prompt("Enter a word of sentence, and I will count number of vowels:");
-Count_Vowels(string)
+var input = prompt("Enter a word of sentence, and I will count number of vowels:");
+Count_Vowels(input)
